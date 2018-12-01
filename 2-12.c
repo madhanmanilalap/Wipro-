dda9169a67303c783e57a188ac594c2e68ca3f29
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-12-01 13:48:33 +0530

    Create 2-12.c

---
+++ 2-12.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,x,b,y;
+printf("Enter the Value of a:");
+scanf("%d",&a);
+printf("Enter the Value of b:");
+scanf("%d",&b);
+printf("Enter the Value of x:");
+scanf("%d",&x);
+y=((a*x)+b);
+printf("%d",y);
+return 0;
+}
